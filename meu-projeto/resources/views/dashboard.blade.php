fcb1409e957afb01af9bec2a6593a767b642ac63
dm201085 <debora.dantas10@aluno.ce.gov.br>
2026-06-22 23:02:12 -0300

    ajeitanto a porta

--- meu-projeto/resources/views/dashboard.blade.php
+++ meu-projeto/resources/views/dashboard.blade.php
@@ -7,31 +7,25 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white p-6 shadow sm:rounded-lg">
+                <h3 class="text-lg font-bold text-gray-700 mb-6 border-b pb-2">Gerenciamento do Sistema</h3>
 
-            <div class="bg-white p-6 shadow">
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+                    
+                    <a href="{{ route('adversarios.index') }}" class="flex items-center justify-center bg-red-600 hover:bg-red-700 text-white font-semibold py-3 px-4 rounded shadow transition duration-200">
+                        CRUD Adversários
+                    </a>
 
-                <h3>Gerenciamento</h3>
+                    <a href="{{ route('ingressos.index') }}" class="flex items-center justify-center bg-black hover:bg-gray-900 text-white font-semibold py-3 px-4 rounded shadow transition duration-200">
+                        CRUD Ingressos
+                    </a>
 
-                <br>
+                    <a href="{{ route('forma-pagamentos.index') }}" class="flex items-center justify-center bg-amber-500 hover:bg-amber-600 text-white font-semibold py-3 px-4 rounded shadow transition duration-200">
+                        CRUD Forma de Pagamento
+                    </a>
 
-                <a href="{{ route('adversarios.index') }}">
-                    CRUD Adversários
-                </a>
-
-                <br><br>
-
-                <a href="{{ route('ingressos.index') }}">
-                    CRUD Ingressos
-                </a>
-
-                <br><br>
-
-                <a href="{{ route('forma-pagamentos.index') }}">
-                    CRUD Forma de Pagamento
-                </a>
-
+                </div>
             </div>
-
         </div>
     </div>
 </x-app-layout>
